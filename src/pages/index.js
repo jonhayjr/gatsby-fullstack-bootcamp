59989bb2078292596bd6c86a3f03cfb48e91c597
Jon Hay <33227538+jonhayjr@users.noreply.github.com>
2022-05-17 07:48:09 -0700

    Removed contact link on Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const Home = () => {
         <Head title="Home"/>
         <h1>Hello</h1>
         <h2>I'm Jon, a React developer living in beautiful, Santa Barbara, California.</h2>
-        <p>Need a developer?  <Link to="/contact">Contact me.</Link></p>
       </Layout>
     </div>
   )
